e7ddeb86ccad7923bc60b45a7198b00c5ce1375e
Kirkov22 <timothy.j.schofield@gmail.com>
2013-12-03 22:14:07 -0800

    New job scraper project

---
+++ Job_Scraper/scrape.rb
@@ -0,0 +1,19 @@
+require 'open-uri'
+require 'Nokogiri'
+
+# URL for Craigslist Tech Support Jobs
+url = 'http://portland.craigslist.org/tch/'
+
+# Collect page data with Nokogiri
+page_data = Nokogiri::HTML(open(url))
+
+# Build date string ([Abbreviate month] [Blank Padded Day])
+yesterday = (Time.now - 86400).strftime('%b %e')
+
+jobs = page_data.css('.row')
+
+yesterdays_jobs = []
+jobs.each do |job|
+  yesterdays_jobs << job.at_css('.pl') if job.at_css('.date').text == yesterday
+end
+
