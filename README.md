a361fb762a6e37254eb15f2f14580342eea60062
Tim Schofield <timothy.j.schofield@gmail.com>
2014-02-18 21:53:35 -0800

    Updated StarboundServerCLI - Modified Structure
    Adjusted StarboundServerCLI to move to a more object oriented
    structure.
    
    Updated main README file

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Folders:
 gem  
 -RubyMonk\ - Problems from the lessons on http://www.rubymonk.com  
 -RubyWarrior\ - Beating the final level of https://www.bloc.io/ruby-warrior/  
+-StarboundServerCLI\ - CLI to modify the server parameters of the
+[Starbound][http://playstarbound.com]  
--- StarboundServerCLI/sbserve.rb
+++ StarboundServerCLI/sbserve.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
 # A script that will modify the Starbound Server file
+# Author: Tim Schofield
 require 'json'
 require 'optparse'
 
-options = {}
+#############
+# Constants #
+#############
 
 # Formatting options for JSON output
 JSON_OPTS = {
@@ -13,140 +16,180 @@ JSON_OPTS = {
   :object_nl => "\n"
 }
 
-# Prints out the current server passwords
-def print_pw(pw_list)
-  puts "Current passwords:"
-  pw_list.each_with_index do |pw, i|
-    puts " #{i + 1}. #{pw}"
+###########
+# Classes #
+###########
+
+class Password_List
+  attr_reader :pw_list
+
+  def initialize(pw_list)
+    @pw_list = pw_list
+  end
+
+  # Prints out the current server passwords
+  def print_list
+    puts "Current passwords:"
+    @pw_list.each_with_index do |pw, i|
+      puts " #{i + 1}. #{pw}"
+    end
+  end
+
+  # Add a new password to the list
+  def add
+    print "Enter new password: "
+    pw = STDIN.gets.chomp
+    @pw_list.push(pw) unless @pw_list.include?(pw)
+  end
+
+  # Delete a password in the list
+  def delete
+    print "Which password to delete? (1-#{@pw_list.length}): "
+    num = STDIN.gets.chomp.to_i - 1
+    @pw_list.delete_at(num)
   end
 end
 
-# Prompts the user for a password option
-def get_pw_opt
-  print "Type 'a' (add), 'd' (del), 'e' (exit): "
-  STDIN.gets.chomp.downcase
-end
+class Starbound_Server
+  attr_reader :options, :changed
 
-# Get the new password to be added
-def get_new_pw
-  print "Enter new password: "
-  STDIN.gets.chomp
-end
+  def initialize
+    # Configure option parsing
+    @options = {}
+    @opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: ./sbserve.rb file1 [options]"
 
-# Get the index of the password to be deleted
-def get_del_index(max)
-  print "Which password to delete? (1-#{max}): "
-  num = STDIN.gets.chomp.to_i - 1
-end
+      opts.on('-h', '--help', 'Displays help') do
+        puts opts
+        exit
+      end
 
-# Display password sub-menu and handle user input
-def password(pw_list)
-  loop do
-    print_pw(pw_list)
-    user_in = get_pw_opt
-    break if user_in == 'e' or user_in == 'exit'
+      opts.on('-w', '--password', 'Edit password list') do
+        @options[:password] = true
+      end
+      
+      opts.on('-n', '--name [NAME]', 'View/Set Server Name') do |name|
+        @options[:name] = name
+      end
 
-    if user_in == 'a' or user_in == 'add'
-      pw_list.push(get_new_pw)
-    elsif user_in == 'd' or user_in == 'del'
-      pw_list.delete_at(get_del_index(pw_list.length))
-    else
-      puts "ERROR: Invalid input - try again."
+      opts.on('-p', '--port [PORT]', 'View/Set Server Port') do |port|
+        @options[:port] = port
+      end
+
+      opts.on('-f', '--port_forward ON/OFF', [:on, :off], 'Turn Port Forwarding On/Off') do |port_fwd|
+        @options[:port_fwd] = true if port_fwd == :on
+        @options[:port_fwd] = false if port_fwd == :off
+      end
     end
 
-    puts ""
-
+    @changed = false
   end
-  pw_list
+
+  def parse(arguments)
+    @opt_parser.parse!(arguments)
+  end
+
+  def load(server_file)
+    # Load server file
+    begin
+      file = File.open(server_file, 'r')
+      @data = JSON.parse(file.read)
+      file.close
+    rescue Errno::ENOENT => e
+      puts "ERROR - #{e.message}"
+      exit
+    end
+  end
+
+  def name
+    if @options[:name].nil?
+      puts "The server's name is: #{@data["serverName"]}."
+    else
+      @data["serverName"] = @options[:name]
+      puts "The server's name has been set to \"#{@data["serverName"]}.\""
+      @changed = true
+    end
+  end
+
+  def port
+    if @options[:port].nil?
+      puts "The server's port is: #{@data["gamePort"]}."
+    else
+      @data["gamePort"] = @options[:port]
+      puts "The server's port has been set to: #{@data["gamePort"]}."
+      @changed = true
+    end
+  end
+
+  def port_fwd
+    @data["upnpPortForwarding"] = @options[:port_fwd]
+    puts "Port forwarding has been turned #{@data["upnpPortForwarding"] ? "on" : "off"}."
+    @changed = true
+  end
+
+  # Print the password list and prompt user for changes
+  def change_passwords
+    pw_list = Password_List.new(@data["serverPasswords"])
+
+    loop do
+      pw_list.print_list
+      print "Enter 'a'/'d'/'e' (add/delete/enter): "
+      input = STDIN.gets.chomp.downcase
+
+      case input
+      when 'a'
+        pw_list.add
+        @changed = true
+      when 'd'
+        pw_list.delete
+        @changed = true
+      when 'e'
+        break
+      else
+        puts "ERROR: Invalid input - try again."
+      end
+      puts ''
+    end
+
+    @data["serverPasswords"] = pw_list
+  end
+
+  def write(filename)
+    begin
+      file = File.open(filename, 'w')
+      file.puts JSON.generate(@data, JSON_OPTS)
+      file.close
+      @changed = false
+    rescue Errno::EACCES => e
+      puts "ERROR - #{e.message}"
+    end
+  end
+end
+
+###############
+# Main Script #
+###############
+
+sb_serve = Starbound_Server.new
+sb_serve.parse(ARGV)
+sb_serve.load(ARGV.first)
+
+# Handle command options on selected server files
+if sb_serve.options.has_key?(:name)
+  sb_serve.name
+end
+
+if sb_serve.options.has_key?(:port)
+  sb_serve.port
+end
+
+if sb_serve.options.has_key?(:port_fwd)
+  sb_serve.port_fwd
+end
+
+if sb_serve.options.has_key?(:password)
+  sb_serve.change_passwords
 end
 
 # Write file
-def write_config(filename, data)
-  begin
-    file = File.open(filename, 'w')
-    file.puts JSON.generate(data, JSON_OPTS)
-    file.close
-  rescue Errno::EACCES => e
-    puts "ERROR - #{e.message}"
-  end
-end
-
-# Parse command options
-optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: ./sbserve.rb file1 [file2 ...] [options]"
-
-  opts.on('-h', '--help', 'Displays help') do
-    puts opts
-    exit
-  end
-
-  opts.on('-w', '--password', 'Edit password list') do
-    options[:password] = true
-  end
-  
-  opts.on('-n', '--name [NAME]', 'View/Set Server Name') do |name|
-    options[:name] = name
-  end
-
-  opts.on('-p', '--port [PORT]', 'View/Set Server Port') do |port|
-    options[:port] = port
-  end
-
-  opts.on('-f', '--port_forward ON/OFF', [:on, :off], 'Turn Port Forwarding On/Off') do |port_fwd|
-    options[:port_fwd] = true if port_fwd == :on
-    options[:port_fwd] = false if port_fwd == :off
-  end
-end
-optparse.parse!
-
-# Handle command options on selected server files
-ARGV.each do |filename|
-
-  # Load file
-  begin
-    file = File.open(filename, 'r')
-    data = JSON.parse(file.read)
-    file.close
-  rescue Errno::ENOENT => e
-    puts "ERROR - #{e.message}"
-    next
-  end
-
-  changed = false
-
-  # Process options
-
-  if options.has_key?(:password)
-    data["serverPasswords"] = password(data["serverPasswords"])
-    changed = true
-  end
-  
-  if options.has_key?(:name)
-    if options[:name].nil?
-      puts "The server's name is: #{data["serverName"]}."
-    else
-      data["serverName"] = options[:name]
-      puts "The server's name has been set to \"#{data["serverName"]}.\""
-      changed = true
-    end
-  end
-
-  if options.has_key?(:port)
-    if options[:port].nil?
-      puts "The server's port is: #{data["gamePort"]}."
-    else
-      data["gamePort"] = options[:port]
-      puts "The server's port has been set to: #{data["gamePort"]}."
-      changed = true
-    end
-  end
-
-  if options.has_key?(:port_fwd)
-    data["upnpPortForwarding"] = options[:port_fwd]
-    puts "Port forwarding has been turned #{data["upnpPortForwarding"] ? "on" : "off"}."
-    changed = true
-  end
-
-  # Write file
-  write_config(filename, data) if changed
-end
+sb_serve.write(ARGV.first) if sb_serve.changed
